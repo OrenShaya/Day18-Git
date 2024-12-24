90f938fe82f91214b819a5e14ac28bf439de7c82
Oren <xxorenxx@gmail.com>
2024-12-24 17:15:18 +0200

    increased ball size

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 'use strict'
 
+var gBallSize = 100
 function onBallClick() {
-  console.log('Ball clicked :)')
+    var ball = document.querySelector('.ball')
+    ball.style.width = (gBallSize + 50) + 'px'
+    ball.style.height = (gBallSize + 50) + 'px'
+    gBallSize += 50
+    ball.innerText = gBallSize
+    
 }
