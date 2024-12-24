102112beab5d52039ae98e0d6248b4d437c68246
Oren <xxorenxx@gmail.com>
2024-12-24 17:39:30 +0200

    Reset ball size when hitting 400

--- main.js
+++ main.js
@@ -2,9 +2,17 @@
 
 var gBallSize = 100
 function onBallClick() {
-    var ball = document.querySelector('.ball')
-    ball.style.width = (gBallSize + 50) + 'px'
-    ball.style.height = (gBallSize + 50) + 'px'
     gBallSize += 50
-    ball.innerText = gBallSize   
+    var ball = document.querySelector('.ball')
+    if (gBallSize > 400) {
+        gBallSize = 100
+        ball.style.width = (gBallSize) + 'px'
+        ball.style.height = (gBallSize) + 'px'
+        ball.innerText = gBallSize
+    } 
+    else {
+        ball.style.width = (gBallSize + 50) + 'px'
+        ball.style.height = (gBallSize + 50) + 'px'
+        ball.innerText = gBallSize
+    }
 }
